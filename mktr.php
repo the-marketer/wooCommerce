4222abcfa58a292333338d996c3bf3385c10482d
Cosmin <cosmin.oancea@themarketer.com>
2026-03-03 14:06:25 +0200

    v1.5.5

--- mktr.php
+++ mktr.php
@@ -3,7 +3,7 @@
  * Plugin Name:             theMarketer - Email marketing, Newsletters, Automation & Loyalty for Woocommerce
  * Plugin URI:              https://themarketer.com/integrations/woocommerce
  * Description:             Automate and transform the way you communicate with WooCommerce customers and cultivate lasting loyalty using your store’s real-time data.
- * Version:                 1.5.4
+ * Version:                 1.5.5
  * Requires at least:       4.6
  * Requires PHP:            5.6
  * Requires Plugins: woocommerce
@@ -53,7 +53,7 @@ if (!defined('MKTR_MAILPOET_SEGMENT')) {
 }
 
 if (!defined('MKTR_VERSION')) {
-    define('MKTR_VERSION', 'v1.5.4');
+    define('MKTR_VERSION', 'v1.5.5');
 }
 
 spl_autoload_register(function ($class) {
